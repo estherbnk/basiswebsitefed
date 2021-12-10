8f17ee2e2c0988e0d3783b8a8ce16af5e51764de
estherbnk <94452324+estherbnk@users.noreply.github.com>
2021-12-10 13:34:39 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,11 @@
 console.log ("hello");
 
-var deButton = document.querySelector("nav button");
+var deButton = document.querySelector("header section button");
+var deNav = document.querySelector("nav ul");
 
 deButton.addEventListener("click", toggleMenu);
 
-function toggleMenu(event) {
-    deNav = even.target.parentNode;
+function toggleMenu() {
     deNav.classList.toggle("toonMenu");
+    deButton.classList.toggle("toonMenu")
 }
